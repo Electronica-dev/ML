121063fc067162b4ac53259c1f343ea9a091944a
Electronica-dev <srsamartha@gmail.com>
2026-01-25 09:55:18 +0530

    fix: format
    - step 4999: train loss 1.0720, val loss 1.4930

--- GPT/smol-gpt/v2.py
+++ GPT/smol-gpt/v2.py
@@ -69,31 +69,31 @@ def estimate_loss():
 
 
 class Head(nn.Module):
-    """ one head of self-attention """
+	""" one head of self-attention """
 
-    def __init__(self, head_size):
-        super().__init__()
-        self.key = nn.Linear(n_embd, head_size, bias=False)
-        self.query = nn.Linear(n_embd, head_size, bias=False)
-        self.value = nn.Linear(n_embd, head_size, bias=False)
-        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+	def __init__(self, head_size):
+		super().__init__()
+		self.key = nn.Linear(n_embd, head_size, bias=False)
+		self.query = nn.Linear(n_embd, head_size, bias=False)
+		self.value = nn.Linear(n_embd, head_size, bias=False)
+		self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
 		self.drouput = nn.Dropout(dropout)
 
-    def forward(self, x):
-        # input of size (batch, time-step, channels)
-        # output of size (batch, time-step, head size)
-        B,T,C = x.shape
-        k = self.key(x)   # (B,T,C)
-        q = self.query(x) # (B,T,C)
-        # compute attention scores ("affinities")
-        wei = q @ k.transpose(-2,-1) * C**-0.5 # (B, T, C) @ (B, C, T) -> (B, T, T)
-        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
-        wei = F.softmax(wei, dim=-1) # (B, T, T)
+	def forward(self, x):
+		# input of size (batch, time-step, channels)
+		# output of size (batch, time-step, head size)
+		B,T,C = x.shape
+		k = self.key(x)   # (B,T,C)
+		q = self.query(x) # (B,T,C)
+		# compute attention scores ("affinities")
+		wei = q @ k.transpose(-2,-1) * C**-0.5 # (B, T, C) @ (B, C, T) -> (B, T, T)
+		wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+		wei = F.softmax(wei, dim=-1) # (B, T, T)
 		wei = self.drouput(wei)
-        # perform the weighted aggregation of the values
-        v = self.value(x) # (B,T,C)
-        out = wei @ v # (B, T, T) @ (B, T, C) -> (B, T, C)
-        return out
+		# perform the weighted aggregation of the values
+		v = self.value(x) # (B,T,C)
+		out = wei @ v # (B, T, T) @ (B, T, C) -> (B, T, C)
+		return out
 
 
 class MultiHeadAttention(nn.Module):
